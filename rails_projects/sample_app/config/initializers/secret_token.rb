b03be9283a9546440836a811cd5544497e2009a7
stollefson <stollefson@madisoncollege.edu>
2013-05-14 17:29:51 -0500

    Finish user edit, update, index, and destroy actions

---
+++ rails_projects/sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'ad757a39b99654683b78371f7bb3c10ead37e51e0f5d9cf3ad620c464b55fae558d7a4fbc5d1a719f812bf6bbb9ccdc513044ee85f401a45c94a151a1a7709e1'
